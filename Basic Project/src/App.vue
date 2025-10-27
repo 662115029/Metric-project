357662240776590a4cd13278ddb7539206d6a652
IdateCMU <wasasart_rat@cmu.ac.th>
2025-10-27 20:06:10 +0700

    get Navbar back to work

--- Basic Project/src/App.vue
+++ Basic Project/src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view />
+  <div>
+    <!-- Navbar แสดงเฉพาะถ้า route ไม่ใช่ login หรือ register -->
+    <NavbarView v-if="showNavbar" />
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import NavbarView from '@/components/Navbar.vue'
+
+const route = useRoute()
+const showNavbar = computed(() => {
+  return !['login', 'register', 'adminlogin'].includes(route.name)
+})
+</script>
